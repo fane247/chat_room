e5783ce9e1f03f4e50fe0d96e0b43246acd6170c
Fane Fonseka <fane247@gmail.com>
2017-06-19 21:11:16 +0100

    added user id to messages

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 
-console.log("linked");
 var messageBox = document.getElementById('messages');
+var sendBox = document.getElementById('send-button');
+sendBox.addEventListener("click", sendMessage, false);
+
+var userIdList = [];
+var userId = 0;
+
+createUserId();
+
 
 
 function sendMessage() {
-	
 
-	var messageContent = createMessageContent();
+	hasText = (document.getElementById("text-box").value) !== "";
 
-	var meesageContainer = createMessageContainer()
+	if (hasText) {
 
-	
+		var messageContent = createMessageContent();
+		var meesageContainer = createMessageContainer();
+		
+		meesageContainer.appendChild(messageContent);
+		messageBox.appendChild(meesageContainer);
 
-	messageContainer.appendChild(messageContent);
+		meesageContainer.animate({opacity: [ 0, 1 ]},800);
 
-
-	messageBox.appendChild(messageContainer);
-
-	document.getElementById("text-box").value = "";
-
+		document.getElementById("text-box").value = "";
+	}
 
 }
 
 function createMessageContent() {
 
+	var name =  document.createTextNode('user ' + userId + ':');
+	var lineBreak = document.createElement('br');
+	var lineBreak2 = document.createElement('br');
 	var text = document.getElementById("text-box").value;
+
 	var newContent = document.createTextNode(text);
 	var messageContent = document.createElement("p");
+	messageContent.appendChild(name);
+	messageContent.appendChild(lineBreak);
+	messageContent.appendChild(lineBreak2);
 	messageContent.appendChild(newContent);
 
 	return messageContent;
@@ -36,5 +50,18 @@ function createMessageContainer(){
 
 	var messageContainer = document.createElement("div");
 	messageContainer.className += "message";
-	
+
+	return messageContainer;
+
+}
+
+function createUserId(){
+
+	id = Math.floor(Math.random() *100000000);
+
+	if (!userIdList.includes(id)) {
+		userIdList.push(id);
+		userId = id;
+	}
+
 }
